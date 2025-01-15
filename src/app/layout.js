bfbd6534af1f5b785d6c78cd70ad1049abaf6959
vikneshoftheleaf <vikneshoftheleaf@gmail.com>
2025-01-15 10:49:13 +0530

    metronome

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,10 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
+      <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
---
+++ src/app/metronome/page.js
@@ -0,0 +1,10 @@
+import MetronomeComponent from "@/components/metronomeComponent"
+export const metadata = {
+    "title": "Metronome",
+    "description":"Practice singing with metronome!"
+}
+export default function Metronome(){
+    return(
+        <MetronomeComponent/>
+    )
+}
